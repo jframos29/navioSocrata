c23a944ca54c2e4cd760af325d5799c6d467aa64
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2019-09-19 00:53:34 -0500

    Otro intento deploy

---
+++ front/build/precache-manifest.dd8ba611c5ece62703258c82ff1e8fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5263fca41b14262f522ba24297f8f5b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d80ebc3168c3fcba1d70",
+    "url": "/static/css/main.fc72e8aa.chunk.css"
+  },
+  {
+    "revision": "5812e555014ced784829",
+    "url": "/static/js/2.ea9d8f00.chunk.js"
+  },
+  {
+    "revision": "d80ebc3168c3fcba1d70",
+    "url": "/static/js/main.694d2120.chunk.js"
+  },
+  {
+    "revision": "9ba826fd182ef2cb5489",
+    "url": "/static/js/runtime~main.cef8c144.js"
+  }
+]);
